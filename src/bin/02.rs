a2ab6906671dcefff6b058614149f5e27e3b341d
Les Ferguson <les.ferguson.42@gmail.com>
2023-11-17 20:42:43 -0600

    fix clippy suggestions

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,41 +1,37 @@
 pub fn part_one(input: &str) -> Option<u32> {
     let mut total_wp: u32 = 0;
-    for present in input.split("\n") {
-        if present.len() == 0 { continue };
-        let mut dimensions: [u32; 3] = [0; 3] ;
-        let mut side: usize = 0;
-        // println!("{}", present);
-        for dim in present.split("x") {
-            // println!("{}", dim);
+    for present in input.split('\n') {
+        if present.is_empty() {
+            continue;
+        };
+        let mut dimensions: [u32; 3] = [0; 3];
+        for (side, dim) in present.split('x').enumerate() {
             let dim_c = dim.trim();
             dimensions[side] = dim_c.parse::<u32>().unwrap();
-            side += 1;
-            // println!("{:?}", dimensions)
-        };
+        }
         dimensions.sort();
-        total_wp += 2*dimensions[0]*dimensions[1]+2*dimensions[1]*dimensions[2]+2*dimensions[0]*dimensions[2]+dimensions[0]*dimensions[1]
-
+        total_wp += 2 * dimensions[0] * dimensions[1]
+            + 2 * dimensions[1] * dimensions[2]
+            + 2 * dimensions[0] * dimensions[2]
+            + dimensions[0] * dimensions[1]
     }
     Some(total_wp)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
     let mut total_r: u32 = 0;
-    for present in input.split("\n") {
-        if present.len() == 0 { continue };
-        let mut dimensions: [u32; 3] = [0; 3] ;
-        let mut side: usize = 0;
-        // println!("{}", present);
-        for dim in present.split("x") {
-            // println!("{}", dim);
+    for present in input.split('\n') {
+        if present.is_empty() {
+            continue;
+        };
+        let mut dimensions: [u32; 3] = [0; 3];
+        for (side, dim) in present.split('x').enumerate() {
             let dim_c = dim.trim();
             dimensions[side] = dim_c.parse::<u32>().unwrap();
-            side += 1;
-            // println!("{:?}", dimensions)
-        };
+        }
         dimensions.sort();
-        total_r += 2*(dimensions[0]+dimensions[1])+dimensions[0]*dimensions[1]*dimensions[2]
-
+        total_r +=
+            2 * (dimensions[0] + dimensions[1]) + dimensions[0] * dimensions[1] * dimensions[2]
     }
     Some(total_r)
 }
